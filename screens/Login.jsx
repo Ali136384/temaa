f87012fb8583552b04786c0d77254ea8d29bd5d3
Ali136384 <haseniali920@gmail.com>
2024-05-11 16:25:59 +0300

    done

--- screens/Login.jsx
+++ screens/Login.jsx
@@ -9,7 +9,6 @@ import {
   TouchableOpacity,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-
 import { Feather } from "@expo/vector-icons";
 import { initializeApp } from "firebase/app";
 import "expo-dev-client";
@@ -26,12 +25,13 @@ import auth from "@react-native-firebase/auth";
 const { height, width } = Dimensions.get("window");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxIN3kQ3IjsHpXHPHT2xjk7PB0iNOlGzw",
-  authDomain: "temaa-auth.firebaseapp.com",
-  projectId: "temaa-auth",
-  storageBucket: "temaa-auth.appspot.com",
-  messagingSenderId: "258186085281",
-  appId: "1:258186085281:web:69dce5f8fda7da9d4ac1e0",
+  apiKey: "AIzaSyCsXLgBeQDDNZgLgSZDMdkcpDixnUOH9-I",
+  authDomain: "temaa-auth-b9466.firebaseapp.com",
+  projectId: "temaa-auth-b9466",
+  storageBucket: "temaa-auth-b9466.appspot.com",
+  messagingSenderId: "543048213631",
+  appId: "1:543048213631:web:9635cdebd4d22d07600b4a",
+  measurementId: "G-J7V3K86KC7",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -97,6 +97,7 @@ export default function Login({ navigation }) {
         console.error("Sign-in error:", error);
       });
   };
+
   return (
     <ImageBackground source={require("../bg1.jpg")}>
       <View style={styles.container}>
